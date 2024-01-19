c0e7a7bfd0705c35896f90a4b00f5c58309978fb
NItsHbAjAJ <146681364+NItsHbAjAJ@users.noreply.github.com>
2024-01-19 13:54:34 -0500

    Update StudentList.java

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -10,7 +10,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * with 5 students from the current section and then iterates over the
  * list to print each name.
  * @author dancye, Jan 2019
- * hello Nitesh
+ * hello Nitesh bajaj
  */
 public class StudentList {
     
