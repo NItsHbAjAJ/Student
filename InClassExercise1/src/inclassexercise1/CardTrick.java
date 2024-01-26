4446bd01f10a64a738f6fe36fb3417d69242fca4
NItsHbAjAJ <niteshbajaj696@gmail.com>
2024-01-26 14:31:54 -0500

    Initial commit

--- InClassExercise1/src/inclassexercise1/CardTrick.java
+++ InClassExercise1/src/inclassexercise1/CardTrick.java
@@ -6,6 +6,9 @@ package inclassexercise1;
 import java.util.Scanner;
 
 /**
+ * 
+ * @ Modifier Nitesh Bajaj Student ID- 991718129
+ * 
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
